28ffd41cfe0e9ea4f04b87bdf3a2b22326a271b4
danirg15 <dani@Danis-MacBook-Pro.local>
2017-09-09 21:15:27 +0200

    Added support to query the year of vehicles. Added gitignore

--- app/app.php
+++ app/app.php
@@ -1,6 +1,4 @@
 <?php
-
-
 require 'sphinxapi.php';
 
 $sphinx = new SphinxClient();
@@ -8,11 +6,22 @@ $sphinx->setServer('10.0.0.10', 9312 );
 $sphinx->setMatchMode( SPH_MATCH_EXTENDED2  );
 $sphinx->setRankingMode(SPH_RANK_PROXIMITY_BM25);
 
+$keywords = $argv[1];
 
-//public bool SphinxClient::setFilterRange ( string $attribute , int $min , int $max [, bool $exclude = false ] )
+//Check if query has car year.
+//If positive the proper filters are applied 
+//in column year_from & year_to
+if(preg_match('/\b\d{4}\b/', $keywords, $year_match)) {
+    $year = $year_match[0];
+    $sphinx->setFilterRange('year_from', $year, 9999, false);
+    $sphinx->setFilterRange('year_to', 0, $year, false);
+
+    //Remove year from query to avoid noise of year value
+    $keywords = str_replace($year, '', $keywords);
+}
+
 $sphinx->setLimits(0, 5);
-$result = $sphinx->query($argv[1]);
-
+$result = $sphinx->query($keywords);
 
 //var_dump($result);
 
